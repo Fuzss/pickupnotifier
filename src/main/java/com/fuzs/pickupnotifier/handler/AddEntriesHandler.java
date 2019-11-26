18890215c06bebe1e06a1b2ada15b12fedd14d1b
Fuzss <MrFusseel@gmail.com>
2019-11-26 10:29:56 +0100

    Performance improvements

--- src/main/java/com/fuzs/pickupnotifier/handler/AddEntriesHandler.java
+++ src/main/java/com/fuzs/pickupnotifier/handler/AddEntriesHandler.java
@@ -57,26 +57,24 @@ public class AddEntriesHandler {
 
     private static void addEntry(DisplayEntry entry) {
 
-        synchronized (PICK_UPS) {
-            float scale = ConfigBuildHandler.DISPLAY_CONFIG.scale.get() / 6.0F;
-            int scaledHeight = (int) (Minecraft.getInstance().mainWindow.getScaledHeight() / scale);
-            int length = (int) (scaledHeight * ConfigBuildHandler.DISPLAY_CONFIG.height.get().floatValue() / DisplayEntry.HEIGHT) - 1;
+        float scale = ConfigBuildHandler.DISPLAY_CONFIG.scale.get() / 6.0F;
+        int scaledHeight = (int) (Minecraft.getInstance().mainWindow.getScaledHeight() / scale);
+        int length = (int) (scaledHeight * ConfigBuildHandler.DISPLAY_CONFIG.height.get().floatValue() / DisplayEntry.HEIGHT) - 1;
 
-            Optional<DisplayEntry> displayEntryOptional = ConfigBuildHandler.GENERAL_CONFIG.combineEntries.get() ? PICK_UPS
-                    .stream().filter(it -> it.canCombine(entry)).findFirst() : Optional.empty();
-            if (displayEntryOptional.isPresent()) {
-                DisplayEntry duplicateEntry = displayEntryOptional.get();
-                duplicateEntry.addCount(entry.getCount());
-                duplicateEntry.resetLife();
-                // adding back to the end of the list
-                PICK_UPS.remove(duplicateEntry);
-                PICK_UPS.add(duplicateEntry);
-            } else {
-                if (PICK_UPS.size() >= length) {
-                    PICK_UPS.remove(0);
-                }
-                PICK_UPS.add(entry);
+        Optional<DisplayEntry> duplicateOptional = ConfigBuildHandler.GENERAL_CONFIG.combineEntries.get() ? PICK_UPS
+                .stream().filter(it -> it.canCombine(entry)).findFirst() : Optional.empty();
+        if (duplicateOptional.isPresent()) {
+            DisplayEntry duplicate = duplicateOptional.get();
+            duplicate.addCount(entry.getCount());
+            duplicate.resetLife();
+            // adding back to the end of the list
+            PICK_UPS.remove(duplicate);
+            PICK_UPS.add(duplicate);
+        } else {
+            if (PICK_UPS.size() >= length) {
+                PICK_UPS.remove(0);
             }
+            PICK_UPS.add(entry);
         }
 
     }
