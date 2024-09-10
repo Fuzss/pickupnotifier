63a06124f990cc4622175d703d67eb5ec0eb762d
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-09-10 21:45:05 +0200

    full 1.21.1 port

--- 1.21.1/Common/src/main/java/fuzs/pickupnotifier/PickUpNotifier.java
+++ 1.21.1/Common/src/main/java/fuzs/pickupnotifier/PickUpNotifier.java
@@ -15,13 +15,13 @@ public class PickUpNotifier implements ModConstructor {
     public static final String MOD_NAME = "Pick Up Notifier";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-    public static final NetworkHandler NETWORK = NetworkHandler.builder(MOD_ID)
-            .optional()
-            .registerLegacyClientbound(S2CTakeItemMessage.class, S2CTakeItemMessage::new)
-            .registerLegacyClientbound(S2CTakeItemStackMessage.class, S2CTakeItemStackMessage::new);
+    public static final NetworkHandler NETWORK = NetworkHandler.builder(MOD_ID).optional().registerLegacyClientbound(
+            S2CTakeItemMessage.class, S2CTakeItemMessage::new).registerLegacyClientbound(S2CTakeItemStackMessage.class,
+            S2CTakeItemStackMessage::new
+    );
     public static final ConfigHolder CONFIG = ConfigHolder.builder(MOD_ID)
             .client(ClientConfig.class)
             .server(ServerConfig.class)
-            .setFileName(ClientConfig.class, id -> ConfigHolder.moveToDir(id, ConfigHolder.defaultName(id, "client")))
-            .setFileName(ServerConfig.class, id -> ConfigHolder.moveToDir(id, ConfigHolder.defaultName(id, "server")));
+            .setFileName(ClientConfig.class, ConfigHolder.getDirectoryNameFactory("client", MOD_ID))
+            .setFileName(ServerConfig.class, ConfigHolder.getDirectoryNameFactory("server", MOD_ID));
 }
